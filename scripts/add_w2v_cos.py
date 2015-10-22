a0f0c91fd53396899b2be349910f0379e643cc42
Carlos Ramisch <carlosramisch@gmail.com>
2015-10-22 22:49:06 +0200

    Add copyright notice on top of all files

--- scripts/add_w2v_cos.py
+++ scripts/add_w2v_cos.py
@@ -1,5 +1,23 @@
 #! /usr/bin/env python
 
+# minimantics: minimalist tool for count-based distributional semantic models
+#
+#    Copyright (C) 2015  Carlos Ramisch, Silvio Ricardo Cordeiro
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
+
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
